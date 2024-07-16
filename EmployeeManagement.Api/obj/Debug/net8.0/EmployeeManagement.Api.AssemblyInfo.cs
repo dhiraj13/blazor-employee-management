0bbdcec3b450fc262ab2da77a556ce52be893e95
Dhiraj Khapangi <dkhapangi13@outlook.com>
2024-07-16 13:39:59 +0545

    Blazor event handling

--- EmployeeManagement.Api/obj/Debug/net8.0/EmployeeManagement.Api.AssemblyInfo.cs
+++ EmployeeManagement.Api/obj/Debug/net8.0/EmployeeManagement.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb141791dbf3f49c20566e78c842584715329938")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ebe4d28f28726d28c4fb76638e07b32385f95fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EmployeeManagement.Web/Components/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.Web/Components/Pages/EmployeeDetailsBase.cs
@@ -1,23 +1,48 @@
 using EmployeeManagement.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace EmployeeManagement.Web.Components.Pages
 {
   public class EmployeeDetailsBase : ComponentBase
   {
+    [Parameter]
+    public string Id { get; set; }
+
     [Inject]
     public IEmployeeService EmployeeService { get; set; }
 
     public Employee Employee { get; set; } = new Employee();
 
-    [Parameter]
-    public string Id { get; set; }
+    protected string Coordinates { get; set; }
+
+    protected string ButtonText { get; set; } = "Hide Footer";
+    protected string CssClass { get; set; } = null;
 
     protected override async Task OnInitializedAsync()
     {
       Id = Id ?? "1";
       Employee = await EmployeeService.GetEmployee(int.Parse(Id));
     }
+
+    protected void Button_Click()
+    {
+      if (ButtonText == "Hide Footer")
+      {
+        ButtonText = "Show Footer";
+        CssClass = "HideFooter";
+      }
+      else
+      {
+        CssClass = null;
+        ButtonText = "Hide Footer";
+      }
+    }
+
+    // protected void Mouse_Move(MouseEventArgs e)
+    // {
+    //   Coordinates = $"X = {e.ClientX} | Y = {e.ClientY}";
+    // }
   }
 }

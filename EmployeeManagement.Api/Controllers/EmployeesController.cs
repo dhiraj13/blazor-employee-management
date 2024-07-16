a2d351a4d94ba95d2fa8d6354034f59d21622ad1
Dhiraj Khapangi <dkhapangi13@outlook.com>
2024-07-16 09:29:15 +0545

    Search in ASP NET Core REST API

--- EmployeeManagement.Api/Controllers/EmployeesController.cs
+++ EmployeeManagement.Api/Controllers/EmployeesController.cs
@@ -18,6 +18,27 @@ namespace EmployeeManagement.Api.Controllers
       this._employeeRepository = employeeRepository;
     }
 
+    [HttpGet("{search}")]
+    public async Task<ActionResult<IEnumerable<Employee>>> Search(string? name, Gender? gender)
+    {
+      try
+      {
+        var result = await _employeeRepository.Search(name, gender);
+
+        if (result.Any())
+        {
+          return Ok(result);
+        }
+
+        return NotFound();
+      }
+      catch (Exception)
+      {
+        return StatusCode(StatusCodes.Status500InternalServerError,
+  "Error retrieving data from the database");
+      }
+    }
+
     [HttpGet]
     public async Task<ActionResult> GetEmployees()
     {
--- EmployeeManagement.Api/Models/EmployeeRepository.cs
+++ EmployeeManagement.Api/Models/EmployeeRepository.cs
@@ -19,6 +19,24 @@ namespace EmployeeManagement.Api.Models
       return await appDbContext.Employees.ToListAsync();
     }
 
+    public async Task<IEnumerable<Employee>> Search(string? name, Gender? gender)
+    {
+      IQueryable<Employee> query = appDbContext.Employees;
+
+      if (!string.IsNullOrEmpty(name))
+      {
+        query = query.Where(e => e.FirstName.Contains(name)
+                    || e.LastName.Contains(name));
+      }
+
+      if (gender != null)
+      {
+        query = query.Where(e => e.Gender == gender);
+      }
+
+      return await query.ToListAsync();
+    }
+
     public async Task<Employee> GetEmployee(int employeeId)
     {
       return await appDbContext.Employees
